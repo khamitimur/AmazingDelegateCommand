af331ee5ee8e2c372ed54735f8574606f8fefaa3
Timur Khamidov <baton237@gmail.com>
2021-04-05 14:30:28 +0300

    Add execute tests

---
+++ Tests/AmazingDelegateCommandTests/Models/AmazingDelegateCommandParameter.swift
@@ -0,0 +1,32 @@
+//
+//  AmazingDelegateCommandParameter.swift
+//  AmazingDelegateCommandTests
+//
+//  Created by t.khamidov on 25.03.2021.
+//
+
+final class AmazingDelegateCommandParameter {
+    
+    // MARK: - Properties
+    
+    let value: Int
+    
+    // MARK: - Initializers
+    
+    init(value: Int) {
+        self.value = value
+    }
+}
+
+final class AnotherAmazingDelegateCommandParameter {
+    
+    // MARK: - Properties
+    
+    let value: Int
+    
+    // MARK: - Initializers
+    
+    init(value: Int) {
+        self.value = value
+    }
+}
