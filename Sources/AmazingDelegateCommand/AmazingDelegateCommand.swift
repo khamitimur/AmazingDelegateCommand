3d7dd09297b55baae8998adeedc3be09615688d8
Timur Khamidov <baton237@gmail.com>
2021-04-05 15:56:57 +0300

    Add documentation

--- Sources/AmazingDelegateCommand/AmazingDelegateCommand.swift
+++ Sources/AmazingDelegateCommand/AmazingDelegateCommand.swift
@@ -5,10 +5,13 @@ public protocol IAmazingDelegateCommand {
     
     // MARK: - Methods
     
+    /// Executes command.
     func execute(_ parameter: Any?)
     
+    /// Returns a value that indicates whether the command can be executed with provided parameter.
     func canExecute(_ parameter: Any?) -> Bool
     
+    /// Adds delegate.
     func addDelegate(_ delegate: AmazingDelegateCommandDelegate)
 }
 
@@ -16,11 +19,15 @@ public final class AmazingDelegateCommand<Target: AnyObject, Parameter: Any>: IA
     
     // MARK: - Type Aliases
     
+    /// Action to be executed.
     public typealias ExecuteAction = (Target) -> (Parameter) -> Void
+    
+    /// Action that return a value that indicates whether the command can be executed with provided parameter.
     public typealias CanExecuteAction = (Target) -> (Parameter) -> Bool
     
     // MARK: - Private Properties
     
+    /// Returns object that owns action to be execute.
     private weak var target: Target?
     
     private let executeAction: ExecuteAction
@@ -30,6 +37,11 @@ public final class AmazingDelegateCommand<Target: AnyObject, Parameter: Any>: IA
     
     // MARK: - Initializers
     
+    /// Initializes a new instance.
+    /// - Parameters:
+    ///   - target: Object that owns action to be execute.
+    ///   - executeAction: Action to be executed.
+    ///   - canExecuteAction: Action that return a value that indicates whether the command can be executed with provided parameter.
     public init(target: Target,
                 executeAction: @escaping ExecuteAction,
                 canExecuteAction: CanExecuteAction? = nil) {
@@ -73,6 +85,7 @@ public final class AmazingDelegateCommand<Target: AnyObject, Parameter: Any>: IA
     
     // MARK: - Public Methods
     
+    /// Raises [canExecuteDidChange](x-source-tag://canExecuteDidChange) method of each delegate.
     public func raiseCanExecuteDidChange() {
         for delegate in delegates {
             delegate.canExecuteDidChange()
@@ -84,10 +97,12 @@ public extension AmazingDelegateCommand where Parameter == Void {
     
     // MARK: - Methods
     
+    /// Executes command.
     func execute() {
         execute(())
     }
     
+    /// Returns a value that indicates whether the command can be executed.
     func canExecute() -> Bool {
         canExecute(())
     }
--- Sources/AmazingDelegateCommand/AmazingDelegateCommandDelegate.swift
+++ Sources/AmazingDelegateCommand/AmazingDelegateCommandDelegate.swift
@@ -2,5 +2,7 @@ public protocol AmazingDelegateCommandDelegate: AnyObject {
     
     // MARK: - Methods
     
+    /// Notifies when command's ability to be executed did change.
+    /// - Tag: canExecuteDidChange
     func canExecuteDidChange()
 }
